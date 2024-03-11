806e4b5fee6287c8ef791068f4b83cb9970aa8e7
Andrew <mr.andrey.kozyrev@gmail.com>
2024-03-11 17:30:14 +0300

    initial commit

---
+++ src/main/java/org/landsreyk/contactswebapp/ContactsWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.landsreyk.contactswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsWebAppApplication.class, args);
+	}
+
+}
